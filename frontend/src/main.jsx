3630cd21ff5796f0a0b8181e9746a44f953adc9b
Ephraim-Munene <ephraimmunene17@gmail.com>
2026-08-22 13:13:30 +0300

    fixed react dom error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDom from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
